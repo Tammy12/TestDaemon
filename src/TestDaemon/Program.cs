2f911427dd0a0ead7a10471a8ac45171e8ed1f34
Xu <U755966@DOW.COM>
2017-05-25 09:35:55 -0400

    first try

---
+++ src/TestDaemon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestDaemon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                // code here
+                Console.WriteLine("I am a service.");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
