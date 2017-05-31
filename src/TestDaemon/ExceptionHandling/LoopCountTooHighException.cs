fd536fbae94b68c354eec1b0f28582c12dedbaa6
Xu <U755966@DOW.COM>
2017-05-31 14:42:52 -0400

    added trycatch

---
+++ src/TestDaemon/ExceptionHandling/LoopCountTooHighException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDaemon.ExceptionHandling
+{
+    public class LoopCountTooHighException: Exception
+    {
+        public LoopCountTooHighException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- src/TestDaemon/Program.cs
+++ src/TestDaemon/Program.cs
@@ -4,52 +4,41 @@ using System.Linq;
 using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
+using TestDaemon.ExceptionHandling;
 
 namespace TestDaemon
 {
     public class Program
     {
-        //private bool _running;
-        //private Task _myLoop;
         private static bool _isRunning = true;
 
         public static void Main(string[] args)
         {
             AssemblyLoadContext.Default.Unloading += Default_Unloading;
-            while (_isRunning)
+            int loopCount = 0;
+            try
             {
-                // code here
-                Console.WriteLine("I am a service.");
-                Thread.Sleep(5000);
+                while (_isRunning)
+                {
+                    // code here
+                    Console.WriteLine("I am a service.");
+                    Thread.Sleep(5000);
+                    loopCount++;
+                    if(loopCount > 10)
+                        throw (new LoopCountTooHighException("Exceeded 10 loops."));
+                }
+            }
+            catch (LoopCountTooHighException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Default_Unloading();
             }
         }
 
-        private static void Default_Unloading(AssemblyLoadContext obj)
+        private static void Default_Unloading(AssemblyLoadContext obj = null)
         {
             _isRunning = false;
             Console.WriteLine("Gracefully stop.");
         }
-
-        //public async Task Start()
-        //{
-        //    await Task.Delay(1);
-        //    _running = true;
-        //    _myLoop = Loop();
-        //}
-
-        //public async Task Stop()
-        //{
-        //    _running = false;
-        //    await _myLoop;
-        //}
-
-        //public async Task Loop()
-        //{
-        //    while(_running)
-        //    {
-        //        await Task.Delay(5000);
-        //        System.Console.WriteLine("I am a service");
-        //    }
-        //}
     }
 }
